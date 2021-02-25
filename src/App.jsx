4eb9e6e590e6e294a57a412173cc71d37574c79c
Syeda Iman Sarwar <69895631+imansarwar@users.noreply.github.com>
2021-02-25 06:31:22 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const App = () => {
+  let newTime = new Date().toLocaleTimeString();
+
+  const [ctime, setCTime] = useState(newTime);
+
+  const UpdateTime = () => {
+    newTime = new Date().toLocaleTimeString();
+    setCTime(newTime);
+  };
+
+  return (
+    <>
+      <h1> {ctime} </h1>
+      <button onClick={UpdateTime}> Get Time </button>
+    </>
+  );
+};
+
+export default App;
